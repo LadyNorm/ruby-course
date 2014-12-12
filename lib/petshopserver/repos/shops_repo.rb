c6a79234fecde827f0a3e8492469d8e998e21779
Nora Neyland <noraleeneyland@gmail.com>
2014-12-12 21:58:13 +0000

    update repos, remove excess info

--- lib/petshopserver/repos/shops_repo.rb
+++ lib/petshopserver/repos/shops_repo.rb
@@ -1,38 +1,7 @@
 module Petshopserver
-
   class ShopsRepo
-    # find a user by user ID. Intended to be used when
-    # someone is already authenticated. We keep their
-    # user id in the cookie.
-    def self.find db, shop_id
-      sql = %q[SELECT * FROM shops WHERE id = $1]
-      result = db.exec(sql, [shop_id])
-      result.first
-    end
-
-    # find shops by name. Intended to be used when
-    # someone tries to sign in.
-    def self.find_by_name db, name
-      sql = %q[SELECT * FROM shops WHERE name = $1]
-      result = db.exec(sql, [name])
-      result.first
-    end
-
-    def self.all(db)
-      db.exec("SELECT * FROM shops").to_a
-    end
-
-    # save shop info
-    def self.save db, shop_data
-      sql = %q[INSERT INTO shops (name) VALUES ($1) RETURNING *]
-      result = db.exec(sql, [shop_data[:name]])
-      result.first
-    end
-
-    def self.destroy db, id
-      sql = %q[DELETE FROM shops WHERE id = $1]
-      db.exec(sql, [id])
-    end
-    
+    def self.get_all_shops(db)
+      shops = db.exec('SELECT * FROM shops').to_a
+    end  
   end
 end
